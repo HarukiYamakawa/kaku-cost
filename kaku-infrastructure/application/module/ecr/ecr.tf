4f3578241dc7e4c89551a2d8684659d352cc5594
HarukiYamakawa <visuhalkey@gmail.com>
2024-02-05 23:40:14 +0900

    FluentBitイメージを作成、ECRリポジトリの作成、イメージのpush

--- kaku-infrastructure/application/module/ecr/ecr.tf
+++ kaku-infrastructure/application/module/ecr/ecr.tf
@@ -79,3 +79,44 @@ resource "aws_ecr_lifecycle_policy" "lifecycle_policy_puma" {
     ]
   })
 }
+
+#fluentbitのECRリポジトリを作成
+resource "aws_ecr_repository" "repository_fluentbit" {
+  name = "${var.name_prefix}-fluentbit"
+
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-fluentbit"
+    group = "${var.tag_group}"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "lifecycle_policy_fluentbit" {
+  repository = aws_ecr_repository.repository_fluentbit.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep at most 3 images",
+        selection = {
+          tagStatus = "any",
+          countType = "imageCountMoreThan",
+          countNumber = 3
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
